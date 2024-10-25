04f6ba049a7c767de6d57568e0f84d80a223706d
Grazen0 <josedanielgrayson@proton.me>
2024-10-24 20:36:39 -0500

    add color usage example + rename stuff

--- src/lib/term_manip.h
+++ src/lib/term_manip.h
@@ -1,8 +1,7 @@
 #ifndef LIB_TERM_MANIP
 #define LIB_TERM_MANIP
 
-namespace term_manip
-{
+namespace term_manip {
     // https://stackoverflow.com/questions/17335816/clear-screen-using-c
     const char CH_CLEAR[] = "\033[2J";
     const char CH_CURSOR_RESET[] = "\033[1;1H";
@@ -10,30 +9,29 @@ namespace term_manip
     void clear_screen();
 
     // https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
-    namespace color
-    {
+    namespace color {
         const char RESET[] = "\033[0m";
         const char BOLD[] = "\033[1m";
         const char FAINT[] = "\033[2m";
         const char ITALIC[] = "\033[3m";
         const char UNDERLINE[] = "\033[4m";
 
-        const char FG_BLACK[] = "\033[30m";
-        const char FG_RED[] = "\033[31m";
-        const char FG_GREEN[] = "\033[32m";
-        const char FG_YELLOW[] = "\033[33m";
-        const char FG_BLUE[] = "\033[34m";
-        const char FG_MAGENTA[] = "\033[35m";
-        const char FG_CYAN[] = "\033[36m";
-        const char FG_WHITE[] = "\033[37m";
-        const char FG_BRIGHT_BLACK[] = "\033[90m";
-        const char FG_BRIGHT_RED[] = "\033[91m";
-        const char FG_BRIGHT_GREEN[] = "\033[92m";
-        const char FG_BRIGHT_YELLOW[] = "\033[93m";
-        const char FG_BRIGHT_BLUE[] = "\033[94m";
-        const char FG_BRIGHT_MAGENTA[] = "\033[95m";
-        const char FG_BRIGHT_CYAN[] = "\033[96m";
-        const char FG_BRIGHT_WHITE[] = "\x1B[97m";
+        const char BLACK[] = "\033[30m";
+        const char RED[] = "\033[31m";
+        const char GREEN[] = "\033[32m";
+        const char YELLOW[] = "\033[33m";
+        const char BLUE[] = "\033[34m";
+        const char MAGENTA[] = "\033[35m";
+        const char CYAN[] = "\033[36m";
+        const char WHITE[] = "\033[37m";
+        const char BRIGHT_BLACK[] = "\033[90m";
+        const char BRIGHT_RED[] = "\033[91m";
+        const char BRIGHT_GREEN[] = "\033[92m";
+        const char BRIGHT_YELLOW[] = "\033[93m";
+        const char BRIGHT_BLUE[] = "\033[94m";
+        const char BRIGHT_MAGENTA[] = "\033[95m";
+        const char BRIGHT_CYAN[] = "\033[96m";
+        const char BRIGHT_WHITE[] = "\x1B[97m";
 
         const char BG_BLACK[] = "\033[40m";
         const char BG_RED[] = "\033[41m";
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 
 int main() {
     term_manip::clear_screen();
+
+    std::cout << term_manip::color::BLUE << term_manip::color::BOLD;
     std::cout << "Hello, world!" << std::endl;
+    std::cout << term_manip::color::RESET;
+
     return 0;
 }
